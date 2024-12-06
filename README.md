6cd2123c2855c572cce6de686a00709391718624
Chloe-adnet-eng <chloe.adnet@ext.bpifrance.fr>
2024-12-06 09:36:15 +0100

    feat(family_members): add first page content with family members

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # secretsanta
 
-## Deployer l'app en local
+## 1. Basic informations 
+
+- Lien du repos github : https://github.com/Chloe-adnet-eng/secretsanta/pull/1/files
+
+
+## 2. Deployer l'app en local
 
 Pour lancer l'app en local : 
 ````
--- secret_santa/app.py
+++ secret_santa/app.py
@@ -1,5 +1,19 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+from pathlib import Path
+
+# Load the data
+
+data_path = Path.cwd() / 'secret_santa' /'data' 
+family_path = data_path / 'family_members.csv'
+family = pd.read_csv(family_path)
+
 
 st.title('🎄 Secret Santa Lavaud, Adnet, Millon 🎄')
+st.write("Hello la famille !")
+
+
+st.write(family)
+st.table(family)
+
+st.dataframe(family.style.highlight_max(axis=0))
