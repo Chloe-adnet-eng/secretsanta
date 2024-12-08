acdc7f359b923001fb5c35ed7143556daab7a8a6
Chloe-adnet-eng <chloe.adnet@ext.bpifrance.fr>
2024-12-08 12:17:26 +0100

    Add final app

--- secret_santa/app.py
+++ secret_santa/app.py
@@ -1,30 +1,70 @@
 import streamlit as st
 import pandas as pd
 from pathlib import Path
-from utils import check_member, clean, get_random_family_member
-from PIL import Image
+from utils import clean
+from firebase_utils import tirage, get_tirage_idee, add_idee
+
+APP_NAME = 'NOEL'
 # Load the data
 
-data_path = Path.cwd() / "secret_santa" /"data" 
+st.title("🎅🎄 Secret Santa 2024")
+st.sidebar.title("La bible du Secret Santa Lavaud-Millon-Adnet 🎅🎄")
+
+with st.sidebar.info("**⭐️Les règles du jeu ⭐️**"):
+    st.markdown("""
+    - Tirer au sort le nom de la personne à qui tu vas offrir un cadeau 🧚‍♀️
+    - Donne des idées de cadeau pour la personne qui te tirera au sort! 🎁
+    """)
+   
+with st.sidebar.success("ℹ️ **PSSSSS: Comment faire les cadeaux**"):
+    st.markdown("""On respecte un budget de 50€ pour les cadeaux du Secret Santa!! 💶""")
+
+st.sidebar.caption("Réalisé par Chloé Adnet")
+
+
+## Get Data
+data_path = Path.cwd() / "secret_santa" / "data" 
 family_path = data_path / "family_members.csv"
 family = pd.read_csv(family_path)
 
-
-st.title("🎄 Secret Santa Lavaud, Adnet, Millon 🎄")
-st.write("Hello la famille !")
-
-pic_path = data_path / "family_picture.jpg"
-image = Image.open(pic_path)
-st.image(image, caption="La famille Lavaud, Adnet, Millon fête noël 🎅", width=320)
+firestore_certificate_path = data_path / "firestore_certificate.json"
+firebase_url = "https://secretsanta-8da89-default-rtdb.firebaseio.com"
 
 first_name = clean(st.text_input("Entre ton prénom:"))
 last_name = clean(st.text_input("Entre ton nom de famille:"))
+identifiant = None
 
-is_member = check_member(first_name, last_name, family)
+if st.button("1️⃣ Click pour enregistrer ton nom et prénom "):
+    identifiant = first_name + last_name
+    st.write("Merci, tu peux continuer! ⭐️")
 
-if st.button("🥁 Tire au sort le nom de la personne à qui tu vas offrir un cadeau"):
-    if not is_member:
-        st.write(f"Tu dois d'abord être membre de la famille pour participer au tirage au sort 🎁")
-    else:
-        random_member = get_random_family_member(family, first_name, last_name)
-        st.write(f"Tu vas offrir un cadeau à {random_member[0].title()} {random_member[1].title()} 🎁")
+
+if st.button("2️⃣ Je tire au sort la personne à laquelle offrir un cadeau"):
+    identifiant = first_name + last_name
+    if identifiant is not None:
+        identifiant_tire = tirage(identifiant)
+        personne = family[family['identifiant'] == identifiant_tire]
+        prenom = personne['prenom'].values[0].title()
+        nom = personne['nom'].values[0].title()
+        st.write(f"Personne tirée :  {prenom} {nom}")
+    else: 
+        st.write("❌ Il faut que tu enregistres ton nom et ton prénom")
+
+st.text("Tu peux ajouter plusieurs idées, à plusieurs moment différent!\n A chaque fois que tu donneras une nouvelle idée, elle sera ajoutée à la liste des précédents!")
+idee = st.text_input("🎁 Mes idées de cadeau")
+if st.button("3️⃣ J'enregistre mes idées de cadeau 🎄"):
+   identifiant = first_name + last_name
+   if identifiant is not None:
+        add_idee(identifiant, idee)
+        st.write("Super, ton idée a bien été notée! 🚀")
+   else: 
+        st.write("❌ Il faut que tu enregistres ton nom et ton prénom")
+
+
+if st.button("4️⃣ Je découvre les idées cadeau de la personne que j'ai tiré!"):
+    identifiant = first_name + last_name
+    if identifiant is not None:
+        idees = get_tirage_idee(identifiant)
+        st.write(idees)
+    else: 
+            st.write("❌ Il faut que tu enregistres ton nom et ton prénom")
---
+++ secret_santa/firebase_utils.py
@@ -0,0 +1,160 @@
+from firebase_admin import credentials, db
+from firebase_admin.db import Reference
+import firebase_admin
+from typing import Optional
+import pandas as pd
+from pathlib import Path
+import random
+
+data_path = Path.cwd() / "secret_santa" / "data" 
+
+FAMILY_PATH = data_path / "family_members.csv"
+FIRESTORE_CERTIFICATE_PATH = data_path / "firestore_certificate.json"
+FIREBASE_URL = "https://secretsanta-8da89-default-rtdb.firebaseio.com"
+
+def delete_app() -> None:
+    firebase_admin.delete_app(firebase_admin.get_app())
+
+def get_app_reference() -> Optional[Reference]:
+    try:    
+        print(f"Trying to get app")
+        firebase_admin.get_app()
+        print(f"Firebase app is already initialized.")
+    except ValueError:
+        print(f"Initializing Firebase app")
+        cred = credentials.Certificate(FIRESTORE_CERTIFICATE_PATH)
+        firebase_admin.initialize_app(
+            cred,
+            {
+                'databaseURL': FIREBASE_URL
+            },
+        )
+        print(f"Firebase app has been initialized.")
+    try :
+        return db.reference()
+    except Exception as e:
+        print(f"The app isn't initialized yet. {e}")
+        return None
+
+def get_all_data() -> dict:
+    
+    app_reference: Reference = get_app_reference()
+    
+    app_data = app_reference.get()
+    
+    delete_app()
+
+    return app_data
+
+def get_data(key:str) -> dict:
+
+    app_reference: Reference = get_app_reference()
+
+    if not app_reference.child(key).get():
+        data = {}
+    else:
+        data = app_reference.child(key).get()
+    
+    delete_app()
+
+    return data
+        
+def write_data(key:str, value:str) -> None:
+
+    app_reference: Reference = get_app_reference()
+
+    if app_reference.child(key).get() is not None:
+        print(f"Key '{key}' exists in the database.")
+    else:
+        app_reference.child(key).set(value)
+        print('Data written successfully!')
+    
+    delete_app()
+
+def update_data(key:str, value:str) -> None:
+    
+    app_reference: Reference = get_app_reference()
+
+    if app_reference.child(key).get() is not None:
+        app_reference.child(key).set(value)
+        print(f"Key '{key}' has been updated.")
+    else:
+        print(f"Key '{key}' does not exist in the database.")
+        
+    delete_app()
+
+def add_idee(identifiant:str, value:str) -> None:
+
+    app_reference: Reference = get_app_reference()
+    key = identifiant + 'cadeau'
+    
+    if app_reference.child(key).get() == "Pas d'idées de cadeau pour le moment! 😭":
+        app_reference.child(key).set("Idée : " + value)
+        
+    elif app_reference.child(key).get() != "Pas d'idées de cadeau pour le moment! 😭":
+        data = app_reference.child(key).get()
+        app_reference.child(key).set(data + ",   " + value)
+
+    delete_app()
+
+def delete_data(key:str) -> None:
+    
+        app_reference: Reference = get_app_reference()
+    
+        if app_reference.child(key).get() is not None:
+            app_reference.child(key).delete()
+            print(f"Key '{key}' has been deleted.")
+        else:
+            print(f"Key '{key}' does not exist in the database.")
+        
+        delete_app()
+
+def delete_all_data() -> None:
+    
+    app_reference: Reference = get_app_reference()
+    
+    app_reference.delete()
+    
+    delete_app()
+
+
+## Tirer au sort un membre de la famille parmi ceux restants
+
+def initiate_secret_santa_data() -> None:
+
+    delete_all_data()
+
+    family = pd.read_csv(FAMILY_PATH)
+
+    for index, row in family.iterrows():
+        write_data(row['identifiant'] + 'cadeau', "Pas d'idées de cadeau pour le moment! 😭")
+        write_data(row['identifiant'] + 'tirage', "")
+        write_data(row['identifiant'] + 'est_deja_tire', "non")
+        write_data(row['identifiant'] + 'a_tire', "non")
+
+def tirage(identifiant):
+
+    family = pd.read_csv(FAMILY_PATH)
+    # si la personne a deja tire : impossible de retirer 
+    if get_data(identifiant + 'a_tire') == "oui":
+        return get_data(identifiant+'tirage')
+    
+    if get_data(identifiant + 'a_tire') == "non":
+        personnes_restantes = []
+        for index, row in family.iterrows():
+            if get_data(row['identifiant'] + 'est_deja_tire') == "non":
+                personnes_restantes.append(row['identifiant'])
+        identifiant_tire = random.choice(personnes_restantes)
+
+        update_data(identifiant + 'tirage', identifiant_tire)
+        update_data(identifiant + 'a_tire', "oui")
+        update_data(identifiant_tire + 'est_deja_tire', "oui")
+        return identifiant_tire
+
+def get_tirage_idee(identifiant):
+    tirage = get_data(identifiant + 'tirage')
+
+    cadeaux = get_data(tirage + 'cadeau')
+    
+    return cadeaux
+
